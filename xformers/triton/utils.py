22fe6883a02ca7e112200d62555ebb37c330f12e
Diana Liskovich <dianaml@fb.com>
2022-10-13 14:48:02 +0000

    update triton flash unit test

--- xformers/triton/utils.py
+++ xformers/triton/utils.py
@@ -41,7 +41,25 @@ def assert_almost_equal(x, y, decimal=2, err_msg=""):
     import numpy.testing as npt
 
     if isinstance(x, torch.Tensor):
+        if x.dtype == torch.bfloat16:
+            x = x.float()
         x = x.cpu().detach().numpy()
     if isinstance(y, torch.Tensor):
+        if y.dtype == torch.bfloat16:
+            y = y.float()
         y = y.cpu().detach().numpy()
     npt.assert_array_almost_equal(x, y, err_msg=err_msg, decimal=decimal)
+
+
+def assert_allclose(x, y, rtol=1e-07, atol=0, err_msg=""):
+    import numpy.testing as npt
+
+    if isinstance(x, torch.Tensor):
+        if x.dtype == torch.bfloat16:
+            x = x.float()
+        x = x.cpu().detach().numpy()
+    if isinstance(y, torch.Tensor):
+        if y.dtype == torch.bfloat16:
+            y = y.float()
+        y = y.cpu().detach().numpy()
+    npt.assert_allclose(x, y, rtol=rtol, atol=atol, err_msg=err_msg)
